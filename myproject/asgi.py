085962c35ca215d9914327670584b06de9c3de42
Omkar Thouta <omkar.thouta@joshtechnologygroup.com>
2022-05-03 00:23:57 +0530

    asgi file changed

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -8,16 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
-from django.conf import settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 import chat.routing
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-settings.configure()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
