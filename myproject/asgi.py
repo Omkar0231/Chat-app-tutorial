a70f21abaa5c4229f13820c9ff7f45db3f232841
Omkar Thouta <omkar.thouta@joshtechnologygroup.com>
2022-05-03 00:36:54 +0530

    final asgi file changed

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
