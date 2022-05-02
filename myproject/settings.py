a12a7705b7ff248bb9707fe0f5c39653489d7705
Omkar Thouta <omkar.thouta@joshtechnologygroup.com>
2022-05-02 21:48:22 +0530

    updated db settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,10 +76,21 @@ ASGI_APPLICATION = 'myproject.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'channelsdb',
+        'USER': 'omkar',
+        'PASSWORD': '2311',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
