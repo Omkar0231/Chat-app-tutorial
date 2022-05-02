92022e6d8bf12b287b99af8885c74c7506814524
Omkar Thouta <omkar.thouta@joshtechnologygroup.com>
2022-05-02 22:13:14 +0530

    updated static files

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static-files/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
