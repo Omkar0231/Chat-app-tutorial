19994d5d311b8f7a1078d17080e534b2ae4953ed
Omkar Thouta <omkar.thouta@joshtechnologygroup.com>
2022-05-03 00:07:09 +0530

    changed channel layer settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -143,9 +143,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
-        # 'CONFIG': {
-        #     'hosts': [('127.0.0.1', 6379)],
-        # }
+        # 'BACKEND': 'channels.layers.InMemoryChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        }
     }
 }
